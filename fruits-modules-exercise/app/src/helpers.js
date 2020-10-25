0370f4017d2074586ed11eb77ff87658793fc93b
nathan <spuddie1984@hotmail.com>
2020-10-25 21:27:41 +0800

    Fruit Modules Exercise

--- fruits-modules-exercise/app/src/helpers.js
+++ fruits-modules-exercise/app/src/helpers.js
@@ -5,3 +5,19 @@
  * remove(items,item):removes the first matching item from items, if item exists and returns it. 
  * Otherwise returns Undefined.
 */
+
+// returns a randomly selected item from an array of items
+function choice(items) {
+    return items[Math.floor(Math.random() * items.length)];
+}
+
+// removes the first matching item from items, if item exists and returns it. 
+// Otherwise returns Undefined.
+function remove(items,item) {
+    return items.find((itemInArr,itemIndex) => {
+        return itemInArr === item ? items.splice(itemIndex, 1) : undefined;
+    })
+}
+
+
+export {choice, remove};
--- fruits-modules-exercise/app/src/index.js
+++ fruits-modules-exercise/app/src/index.js
@@ -9,3 +9,20 @@
  * Log the message "I'm sorry, we're all out. We have FRUITSLEFT left"
  * 
  */
+
+import {choice, remove} from './helpers';
+import foods from './foods';
+
+let randomFruit = choice(foods);
+
+
+console.log(`I'd like one ${randomFruit}, please!`);
+
+console.log(`Here you go: ${randomFruit}`);
+
+console.log(`Delicious may I have another?`);
+
+remove(foods,randomFruit);
+
+console.log(foods.length);
+console.log(`I'm sorry, we're all out. We have ${foods} left`)
